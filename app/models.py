4ff7765e15dcf0602578ae443b6d75580f1c8bf7
Clint Daniels <clint.daniels@sandag.org>
2016-07-01 09:58:45 -0700

    Code cleanup.... How did it get this disorganized???

--- app/models.py
+++ app/models.py
@@ -1,15 +1,226 @@
-from . import db, zoning_definitions, allowed_use_def, development_type_defs,model_structure_def, login_manager
-from werkzeug.security import generate_password_hash, check_password_hash
+"""  Module for holding together ORM Mappings """
+
 from flask.ext.login import UserMixin
 from geoalchemy2.shape import to_shape
 from geoalchemy2.types import Geography
 from shapely.geometry import mapping as geojson_mapping
+from werkzeug.security import generate_password_hash, check_password_hash
 
+from . import db, zoning_definitions, allowed_use_def, development_type_defs, model_structure_def, login_manager
+
+
+class AllowedUse(db.Model):
+    """ SQL Alchemy Class that maps to urbansim.zoning_allowed_use """
+
+    __tablename__ = 'zoning_allowed_use'
+    __table_args__ = {'schema': 'urbansim'}
+    zoning_allowed_use_id = db.Column(db.Integer, primary_key=True)
+    zoning_id = db.Column(db.Integer, db.ForeignKey('urbansim.zoning.zoning_id'))
+    development_type_id = db.Column(db.Integer, db.ForeignKey('ref.development_type.development_type_id'))
+
+    @property
+    def to_json(self):
+        return {
+            'zoning_allowed_use_id': self.zoning_allowed_use_id,
+            'zoning_id': self.zoning_id,
+            'zone_code': self.zoning.zone_code,
+            'development_type_id': self.development_type_id,
+            'development_type': self.development_type.name
+        }
+
+    @staticmethod
+    def insert_allowed_use():
+        for a in allowed_use_def:
+            allowed_use = AllowedUse.query.filter_by(zoning_allowed_use_id=a).first()
+            if allowed_use is None:
+                allowed_use = AllowedUse(zoning_allowed_use_id=a)
+            allowed_use.zoning = Zoning.query.filter_by(zoning_id=allowed_use_def[a]['zoning_id']).first()
+            allowed_use.development_type_id = allowed_use_def[a]['development_type_id']
+            db.session.add(allowed_use)
+        db.session.commit()
+
+    def __repr__(self):
+        return '<AllowedUse %r-%r>' % (self.zoning_id, self.development_type_id)
+
+
+class Building(db.Model):
+    """ SQL Alchemy Class that maps to urbansim.buildings """
+
+    __tablename__ = 'buildings'
+    __table_args__ = {'schema': 'urbansim'}
+    column_default_sort = 'building_id'
+    building_id = db.Column(db.Integer, primary_key=True)
+    development_type_id = db.Column(db.Integer, nullable=False)
+    parcel_id = db.Column(db.Integer, db.ForeignKey('urbansim.parcels.parcel_id'))
+    improvement_value = db.Column(db.Float)
+    residential_units = db.Column(db.SmallInteger)
+    residential_sqft = db.Column(db.Integer)
+    non_residential_sqft = db.Column(db.Integer)
+    job_spaces = db.Column(db.SmallInteger)
+    jobs = db.relationship('Job', backref='building', lazy='dynamic')
+    non_residential_rent_per_sqft = db.Column(db.Float)
+    residential_rent_per_sqft = db.Column(db.Float, name='price_per_sqft')
+    stories = db.Column(db.Integer)
+    year_built = db.Column(db.SmallInteger)
+    shape = db.Column(Geography(geometry_type='GEOMETRY', srid=4236))
+    centroid = db.Column(Geography(geometry_type='POINT', srid=4326))
+
+    @property
+    def shape_geojson(self):
+        return geojson_mapping(to_shape(self.shape)) if self.shape is not None else None
+
+    @property
+    def centroid_geojson(self):
+        return geojson_mapping(to_shape(self.centroid)) if self.shape is not None else None
+
+    @property
+    def to_json(self):
+        return {
+            'building_id': self.building_id,
+            'parcel_id': self.parcel_id,
+            'development_type_id': self.development_type_id,
+            'improvement_value': '{:,}'.format(
+                int(self.improvement_value)) if self.improvement_value is not None else '',
+            'residential_units': '{:,}'.format(self.residential_units) if self.residential_units is not None else '',
+            'residential_sqft': '{:,}'.format(self.residential_sqft) if self.residential_sqft is not None else '',
+            'non_residential_sqft': '{:,}'.format(
+                self.non_residential_sqft) if self.non_residential_sqft is not None else '',
+            'job_spaces': '{:,}'.format(self.job_spaces) if self.job_spaces is not None else '',
+            'non_residential_rent_per_sqft': '{:,}'.format(
+                self.non_residential_rent_per_sqft) if self.non_residential_rent_per_sqft is not None else '',
+            'residential_rent_per_sqft': '{:,}'.format(
+                self.residential_rent_per_sqft) if self.non_residential_rent_per_sqft is not None else '',
+            'stories': '{:,}'.format(self.stories) if self.job_spaces is not self.stories else '',
+            'year_built': self.year_built,
+            'DT_RowId': 'row_%i' % self.building_id
+        }
+
+    def __repr__(self):
+        return '<Building %r>' % self.building_id
+
+
+class DevelopmentType(db.Model):
+    """ SQL Alchemy Class that maps to a lookup table ref.development_type """
+
+    __tablename__ = 'development_type'
+    __table_args__ = {'schema': 'ref'}
+    development_type_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(35), unique=True, nullable=False)
+    allowed_uses = db.relationship('AllowedUse', backref='development_type', lazy='dynamic')
+
+    @staticmethod
+    def insert_development_type():
+        for d in development_type_defs:
+            development_type = DevelopmentType.query.get(d)
+            if development_type is None:
+                development_type = DevelopmentType(development_type_id=d)
+            development_type.name = development_type_defs[d]
+            db.session.add(development_type)
+        db.session.commit()
+
+    def __repr__(self):
+        return '<DevelopmentType %r-%r>' % (self.development_type_id, self.name)
+
+
+class Geographies(db.Model):
+    """ SQL Alchemy Class that maps to the common geography shape table ref.geography_zone """
+
+    __tablename__ = 'geography_zone'
+    __table_args__ = {'schema': 'ref'}
+    geography_zone_id = db.Column(db.Integer, primary_key=True)
+    geography_type = db.Column(db.String(8), nullable=False)
+    zone = db.Column(db.Integer, nullable=False)
+    shape = db.Column(Geography(geometry_type='MULTIPOLYGON', srid=4326), nullable=False)
+    centroid = db.Column(Geography(geometry_type='POINT', srid=4326), nullable=False)
+
+
+class Job(db.Model):
+    """ SQL Alchemy Class that maps to urbansim.jobs """
+
+    __tablename__ = 'jobs'
+    __table_args__ = {'schema': 'urbansim'}
+    job_id = db.Column(db.Integer, primary_key=True)
+    sector_id = db.Column(db.Integer, nullable=False)
+    building_id = db.Column(db.Integer, db.ForeignKey('urbansim.buildings.building_id'))
+
+
+class Jurisdiction(db.Model):
+    """ SQL Alchemy Class that maps to ref.jurisdiction """
+
+    __tablename__ = 'jurisdiction'
+    __table_args__ = {'schema': 'ref'}
+    jurisdiction_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32), unique=True)
+    zones = db.relationship('Zoning', backref='jurisdiction', lazy='dynamic')
+
+    @staticmethod
+    def insert_jurisdictions():
+        jurisdictions = {
+            1: 'Carlsbad',
+            2: 'Chula Vista',
+            3: 'Coronado',
+            4: 'Del Mar'
+        }
+        for j in jurisdictions:
+            jurisdiction = Jurisdiction.query.filter_by(jurisdiction_id=j).first()
+            if jurisdiction is None:
+                jurisdiction = Jurisdiction(jurisdiction_id=j)
+            jurisdiction.name = jurisdictions[j]
+            db.session.add(jurisdiction)
+        db.session.commit()
+
+    def __repr__(self):
+        return '<Jurisdiction %r>' % self.name
+
+
+class ModelStructure(db.Model):
+    """ SQL Alchemy Class that maps to ref.model """
+
+    __tablename__ = 'model'
+    __table_args__ = {'schema': 'ref'}
+    model_id = db.Column(db.SmallInteger, primary_key=True)
+    name = db.Column(db.String(64), unique=True, nullable=False)
+    display_name = db.Column(db.String(64), unique=True, nullable=False)
+    overview = db.Column(db.String())
+    active = db.Column(db.Boolean(), nullable=False, default=False)
+
+    @staticmethod
+    def insert_model_structure():
+        for m in model_structure_def:
+            model_structure = ModelStructure.query.get(m)
+            if model_structure is None:
+                model_structure = ModelStructure(model_id=m)
+            model_structure.name = m.name
+            model_structure.display_name = m.display_name
+            model_structure.overview = m.overview
+            model_structure.active = m.active
+            db.session.add(model_structure)
+        db.session.commit()
+
+    def __repr__(self):
+        return '<ModelStructure %r-%r>' % (self.model_id, self.name)
+
+
+class Parcel(db.Model):
+    """ SQL Alchemy Class that maps to urbansim.parcels """
+
+    __tablename__ = 'parcels'
+    __table_args__ = {'schema': 'urbansim'}
+    parcel_id = db.Column(db.Integer, primary_key=True)
+    msa_id = db.Column(db.Integer)
+    luz_id = db.Column(db.Integer)
+    mgra_id = db.Column(db.Integer)
+    buildings = db.relationship('Building', backref='parcel', lazy='dynamic')
+
+    def __repr__(self):
+        return '<Parcel %r>' % self.parcel_id
 
 
 class User(UserMixin, db.Model):
+    """ SQL Alchemy Class that maps to ref.users """
+
     __tablename__ = 'users'
-    __table_args__ = {'schema' : 'ref'}
+    __table_args__ = {'schema': 'ref'}
     id = db.Column(db.Integer, primary_key=True, name='users_id')
     email = db.Column(db.String(64), unique=True, nullable=False, index=True)
     password_hash = db.Column(db.String(128))
@@ -58,39 +269,15 @@ class User(UserMixin, db.Model):
     def __repr__(self):
         return '<User %r-%r>' % (self.id, self.email)
 
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
 
-class Jurisdiction(db.Model):
-    __tablename__ = 'jurisdiction'
-    __table_args__ = {'schema': 'ref'}
-    jurisdiction_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(32), unique=True)
-    zones = db.relationship('Zoning', backref='jurisdiction', lazy='dynamic')
-
-    @staticmethod
-    def insert_jurisdictions():
-        jurisdictions = {
-            1: 'Carlsbad',
-            2: 'Chula Vista',
-            3: 'Coronado',
-            4: 'Del Mar'
-        }
-        for j in jurisdictions:
-            jurisdiction = Jurisdiction.query.filter_by(jurisdiction_id=j).first()
-            if jurisdiction is None:
-                jurisdiction = Jurisdiction(jurisdiction_id=j)
-            jurisdiction.name = jurisdictions[j]
-            db.session.add(jurisdiction)
-        db.session.commit()
-
-    def __repr__(self):
-        return '<Jurisdiction %r>' % self.name
-
-
 class Zoning(db.Model):
+    """ SQL Alchemy Class that maps to urbansim.zoning """
+
     __tablename__ = 'zoning'
     __table_args__ = {'schema': 'urbansim'}
 
@@ -126,7 +313,8 @@ class Zoning(db.Model):
             zoning = Zoning.query.filter_by(zoning_id=z).first()
             if zoning is None:
                 zoning = Zoning(zoning_id=z)
-            zoning.jurisdiction = Jurisdiction.query.filter_by(jurisdiction_id=zoning_definitions[z]['jurisdiction_id']).first()
+            zoning.jurisdiction = Jurisdiction.query.filter_by(
+                jurisdiction_id=zoning_definitions[z]['jurisdiction_id']).first()
             zoning.zone_code = zoning_definitions[z]['zone_code']
             zoning.region_id = zoning_definitions[z]['region_id']
             zoning.min_far = zoning_definitions[z]['min_far']
@@ -143,162 +331,3 @@ class Zoning(db.Model):
 
     def __repr__(self):
         return '<Zoning %r>' % self.zoning_id
-
-
-class AllowedUse(db.Model):
-    __tablename__ = 'zoning_allowed_use'
-    __table_args__ = {'schema': 'urbansim'}
-    zoning_allowed_use_id = db.Column(db.Integer, primary_key=True)
-    zoning_id = db.Column(db.Integer, db.ForeignKey('urbansim.zoning.zoning_id'))
-    development_type_id = db.Column(db.Integer, db.ForeignKey('ref.development_type.development_type_id'))
-
-    @property
-    def to_json(self):
-        return {
-            'zoning_allowed_use_id': self.zoning_allowed_use_id,
-            'zoning_id': self.zoning_id,
-            'zone_code': self.zoning.zone_code,
-            'development_type_id': self.development_type_id,
-            'development_type': self.development_type.name
-        }
-
-    @staticmethod
-    def insert_allowed_use():
-        for a in allowed_use_def:
-            allowed_use = AllowedUse.query.filter_by(zoning_allowed_use_id=a).first()
-            if allowed_use is None:
-                allowed_use = AllowedUse(zoning_allowed_use_id=a)
-            allowed_use.zoning = Zoning.query.filter_by(zoning_id=allowed_use_def[a]['zoning_id']).first()
-            allowed_use.development_type_id = allowed_use_def[a]['development_type_id']
-            db.session.add(allowed_use)
-        db.session.commit()
-
-    def __repr__(self):
-        return '<AllowedUse %r-%r>' % (self.zoning_id, self.development_type_id)
-
-
-class DevelopmentType(db.Model):
-    __tablename__ = 'development_type'
-    __table_args__ = {'schema': 'ref'}
-    development_type_id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(35), unique=True, nullable=False)
-    allowed_uses = db.relationship('AllowedUse', backref='development_type', lazy='dynamic')
-
-    @staticmethod
-    def insert_development_type():
-        for d in development_type_defs:
-            development_type = DevelopmentType.query.get(d)
-            if development_type is None:
-                development_type = DevelopmentType(development_type_id=d)
-            development_type.name = development_type_defs[d]
-            db.session.add(development_type)
-        db.session.commit()
-
-    def __repr__(self):
-        return '<DevelopmentType %r-%r>' % (self.development_type_id, self.name)
-
-
-class ModelStructure(db.Model):
-    __tablename__ = 'model'
-    __table_args__ = {'schema': 'ref'}
-    model_id = db.Column(db.SmallInteger, primary_key=True)
-    name = db.Column(db.String(64), unique=True, nullable=False)
-    display_name = db.Column(db.String(64), unique=True, nullable=False)
-    overview = db.Column(db.String())
-    active = db.Column(db.Boolean(), nullable=False, default=False)
-
-    @staticmethod
-    def insert_model_structure():
-        for m in model_structure_def:
-            model_structure = ModelStructure.query.get(m)
-            if model_structure is None:
-                model_structure = ModelStructure(model_id=m)
-            model_structure.name = m.name
-            model_structure.display_name = m.display_name
-            model_structure.overview = m.overview
-            model_structure.active = m.active
-            db.session.add(model_structure)
-        db.session.commit()
-
-    def __repr__(self):
-        return '<ModelStructure %r-%r>' %(self.model_id, self.name)
-
-
-class Building(db.Model):
-    __tablename__ = 'buildings'
-    __table_args__ = {'schema': 'urbansim'}
-    column_default_sort = 'building_id'
-    building_id = db.Column(db.Integer, primary_key=True)
-    development_type_id = db.Column(db.Integer, nullable=False)
-    parcel_id = db.Column(db.Integer, db.ForeignKey('urbansim.parcels.parcel_id'))
-    improvement_value = db.Column(db.Float)
-    residential_units = db.Column(db.SmallInteger)
-    residential_sqft = db.Column(db.Integer)
-    non_residential_sqft = db.Column(db.Integer)
-    job_spaces = db.Column(db.SmallInteger)
-    jobs = db.relationship('Job', backref='building', lazy='dynamic')
-    non_residential_rent_per_sqft = db.Column(db.Float)
-    residential_rent_per_sqft = db.Column(db.Float, name='price_per_sqft')
-    stories = db.Column(db.Integer)
-    year_built = db.Column(db.SmallInteger)
-    shape = db.Column(Geography(geometry_type='GEOMETRY', srid=4236))
-    centroid = db.Column(Geography(geometry_type='POINT', srid=4326))
-
-    @property
-    def shape_geojson(self):
-        return geojson_mapping(to_shape(self.shape)) if self.shape is not None else None
-
-    @property
-    def centroid_geojson(self):
-        return geojson_mapping(to_shape(self.centroid)) if self.shape is not None else None
-
-    @property
-    def to_json(self):
-        return {
-            'building_id': self.building_id,
-            'parcel_id': self.parcel_id,
-            'development_type_id': self.development_type_id,
-            'improvement_value': '{:,}'.format(int(self.improvement_value)) if self.improvement_value is not None else '',
-            'residential_units': '{:,}'.format(self.residential_units) if self.residential_units is not None else '',
-            'residential_sqft': '{:,}'.format(self.residential_sqft) if self.residential_sqft is not None else '',
-            'non_residential_sqft': '{:,}'.format(self.non_residential_sqft) if self.non_residential_sqft is not None else '',
-            'job_spaces': '{:,}'.format(self.job_spaces) if self.job_spaces is not None else '',
-            'non_residential_rent_per_sqft': '{:,}'.format(self.non_residential_rent_per_sqft) if self.non_residential_rent_per_sqft is not None else '',
-            'residential_rent_per_sqft': '{:,}'.format(self.residential_rent_per_sqft) if self.non_residential_rent_per_sqft is not None else '',
-            'stories': '{:,}'.format(self.stories) if self.job_spaces is not self.stories else '',
-            'year_built': self.year_built,
-            'DT_RowId': 'row_%i' % self.building_id
-        }
-
-    def __repr__(self):
-        return '<Building %r>' % self.building_id
-
-
-class Job(db.Model):
-    __tablename__ = 'jobs'
-    __table_args__ = {'schema': 'urbansim'}
-    job_id = db.Column(db.Integer, primary_key=True)
-    sector_id = db.Column(db.Integer, nullable=False)
-    building_id = db.Column(db.Integer, db.ForeignKey('urbansim.buildings.building_id'))
-
-class Parcel(db.Model):
-    __tablename__ = 'parcels'
-    __table_args__ = {'schema': 'urbansim'}
-    parcel_id = db.Column(db.Integer, primary_key=True)
-    msa_id = db.Column(db.Integer)
-    luz_id = db.Column(db.Integer)
-    mgra_id = db.Column(db.Integer)
-    buildings = db.relationship('Building', backref='parcel', lazy='dynamic')
-
-    def __repr__(self):
-        return '<Parcel %r>' % self.parcel_id
-
-
-class Geographies(db.Model):
-    __tablename__ = 'geography_zone'
-    __table_args__ = {'schema': 'ref'}
-    geography_zone_id = db.Column(db.Integer, primary_key=True)
-    geography_type = db.Column(db.String(8), nullable=False)
-    zone = db.Column(db.Integer, nullable=False)
-    shape = db.Column(Geography(geometry_type='MULTIPOLYGON', srid=4326), nullable=False)
-    centroid = db.Column(Geography(geometry_type='POINT', srid=4326), nullable=False)
